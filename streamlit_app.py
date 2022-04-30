a6f67ac22976ea953bac3126340dbf7a2e7dddd2
naren-garapati <104664711+naren-garapati@users.noreply.github.com>
2022-04-30 10:22:55 +0530

    Update streamlit_app.py
    added the pick list and the display table

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,8 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+#pick list
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
+                      
+#diplay table 
+streamlit.dataframe(my_fruit_list)                     
